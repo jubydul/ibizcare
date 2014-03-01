78c8b78531f0e07757ef0989f5422091daad18a9
Mohammed Jubydul Haque <jubydul.rony@gmail.com>
2014-03-01 11:22:48 +0600

    initial commit of ibizcare website
    demo project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Electronic Intelligent Business Care
+
+	Currently Working for version 1.0
+	comprises of - 
+		1. Home page
+		2. About the organization
+	
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Intelligent Business Care~ Home</title>
+</head>
+<body>
+	<img src="images/banner.png" width="172" />
+    <hr />
+	<h1>Welcome to Intelligent Business Care</h1>
+    <p>
+    	We are providing electronic business solutions for all types of business. We have a skilled team.
+        
+    </p>
+    <br/><br/>
+    <hr/>
+    copyright &copy; All rights reserved by eBizSol.
+</body>
+</html>
