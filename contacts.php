f8c2a7e128eb19b4912922c4cebc7382f7dcd02f
Kayes Mohammed Hozaifa <kayeshozaifa@gmail.com>
2014-03-01 12:48:15 +0600

    contacts.php added

---
+++ contacts.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Intelligent Business Care~ Contact</title>
+</head>
+<body>
+	<img src="images/banner.png" width="172" />
+    <hr />
+	<h1>Contact Us</h1>
+    <p>
+    	
+    </p>
+    <br/><br/>
+    <hr/>
+    copyright &copy; All rights reserved by eBizSol.
+</body>
+</html>
