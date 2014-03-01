54284114a69497440f75f04bdf735b30a03eba12
Kayes Mohammed Hozaifa <kayeshozaifa@gmail.com>
2014-03-01 13:23:35 +0600

    r2_contact is ready for release 2.0

--- contacts.php
+++ contacts.php
@@ -9,7 +9,9 @@
     <hr />
 	<h1>Contact Us</h1>
     <p>
-    	67/F, Green Road, Panthapath, Dhaka - 1205
+    	67/F, Green Road, Panthapath, Dhaka - 1205<br/>
+        email: info@ibizcare.com<br/>
+        web:www.ibizcare.com
     </p>
     <br/><br/>
     <hr/>
